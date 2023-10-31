fb0daaff7a4610cd23a650f7799e9cd6f680dabe
Andy <72534502+Sarri09@users.noreply.github.com>
2023-10-31 14:44:46 -0300

    PAID update

--- Kafka Docker/1. Kafka Brokers/inscripcion/producer/producer.py
+++ Kafka Docker/1. Kafka Brokers/inscripcion/producer/producer.py
@@ -1,16 +1,20 @@
 from confluent_kafka import Producer
 
-broker = '172.18.0.4:9092' # verificar ip no siempre se abre aqui
-
+broker = '172.18.0.4:9092' # verificar, la ip no siempre abre aqui
 producer = Producer({'bootstrap.servers': broker})
 topic = 'inscripciones'
 
 nombre = input("Ingresa un nombre: ")
 nombre_tienda = input("Ingresa un nombre de tienda: ")
+is_paid = input("¿PAID? (True/False): ").lower()
 
-mensaje = f"Nombre: {nombre}, Tienda: {nombre_tienda}"
+mensaje = f"Nombre: {nombre}, Tienda: {nombre_tienda}, Es un cliente pago: {is_paid}"
 
-producer.produce(topic, key=None, value=mensaje)
+if is_paid == "true":
+    producer.produce(topic, key="paid", value=mensaje)
+else:
+    producer.produce(topic, key="not_paid", value=mensaje)
 
 producer.flush()
+
 
