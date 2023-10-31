2fdc76cc67549d6bff090cbdff7878712801427b
Andy <72534502+Sarri09@users.noreply.github.com>
2023-10-31 13:42:00 -0300

    consumer

--- Kafka Docker/1. Kafka Brokers/inscripcion/consumer/consumer.py
+++ Kafka Docker/1. Kafka Brokers/inscripcion/consumer/consumer.py
@@ -1,29 +1,49 @@
 from confluent_kafka import Consumer, KafkaError
-import base64
 
-broker = '172.18.0.4:9092'
+# Configuración del consumidor
+conf = {
+    'bootstrap.servers': 'kafka1:9092',  # Cambia a la dirección y puerto correctos si es necesario
+    'group.id': 'proceso-de-aprobacion',
+    'auto.offset.reset': 'earliest'  # Inicializar desde el principio del topic
+}
 
-consumer = Consumer({
-    'bootstrap.servers': broker,
-    'group.id': 'my-group',  
-    'auto.offset.reset': 'earliest'
-})
+# Crear un objeto consumidor
+consumer = Consumer(conf)
 
-consumer.subscribe(['inscripciones'])  
+# Suscribirse al topic "inscripciones"
+consumer.subscribe(['inscripciones'])
 
-while True:
-    msg = consumer.poll(1.0)
+try:
+    while True:
+        # Esperar mensajes
+        msg = consumer.poll(1.0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        if msg.error().code() == KafkaError._PARTITION_EOF:
-            print('Fin de partición alcanzado')
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                print('No more messages')
+            else:
+                print('Error al recibir mensaje: {}'.format(msg.error()))
         else:
-            print('Error en el mensaje: {}'.format(msg.error()))
-    else:
-        mensaje = msg.value()
+            # Decodificar el mensaje como una cadena UTF-8
+            message_str = msg.value().decode('utf-8')
 
-        mensaje_encriptado = base64.b64encode(mensaje.encode()).decode()
+            # Dividir el mensaje en las partes deseadas (usuario y nombre de la tienda)
+            parts = message_str.split(',')
+            if len(parts) >= 2:
+                user_name = parts[0].strip()
+                store_name = parts[1].strip()
 
-        print('Mensaje encriptado: {}'.format(mensaje_encriptado))
+                # Imprimir la información en el formato deseado, eliminando los primeros 8 caracteres
+                print(f'Usuario: {user_name[8:]}')
+                print(f'Clave: {store_name[8:]}')
+                print('---')  # Separador entre mensajes
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Cerrar el consumidor al salir
+    consumer.close()
+
