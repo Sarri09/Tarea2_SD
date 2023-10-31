cbf50888f1d716edac9c5002f1f8d9109069579d
Andy <72534502+Sarri09@users.noreply.github.com>
2023-10-31 12:21:08 -0300

    Update consumer.py

--- Kafka Docker/1. Kafka Brokers/inscripcion/consumer/consumer.py
+++ Kafka Docker/1. Kafka Brokers/inscripcion/consumer/consumer.py
@@ -1,3 +1,29 @@
 from confluent_kafka import Consumer, KafkaError
+import base64
 
+broker = '172.18.0.4:9092'
 
+consumer = Consumer({
+    'bootstrap.servers': broker,
+    'group.id': 'my-group',  
+    'auto.offset.reset': 'earliest'
+})
+
+consumer.subscribe(['inscripciones'])  
+
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            print('Fin de partición alcanzado')
+        else:
+            print('Error en el mensaje: {}'.format(msg.error()))
+    else:
+        mensaje = msg.value()
+
+        mensaje_encriptado = base64.b64encode(mensaje.encode()).decode()
+
+        print('Mensaje encriptado: {}'.format(mensaje_encriptado))
