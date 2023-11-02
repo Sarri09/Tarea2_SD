311525bc7bd7b85abb8466f84c6b504183a327e6
Andy <72534502+Sarri09@users.noreply.github.com>
2023-11-02 16:32:07 -0300

    Update stock.py

--- Kafka Docker/1. Kafka Brokers/Consumer/stock.py
+++ Kafka Docker/1. Kafka Brokers/Consumer/stock.py
@@ -1,21 +1,17 @@
 from confluent_kafka import Consumer, KafkaError
 
-# Configuración del consumidor de Kafka
 config = {
-    'bootstrap.servers': '172.18.0.5:9092',  # Cambia esto por la dirección de tu servidor Kafka
+    'bootstrap.servers': '172.18.0.5:9092',  
     'group.id': 'distribuidores',
     'auto.offset.reset': 'earliest'
 }
 
-# Crea una instancia del consumidor
 consumer = Consumer(config)
 
-# Subscríbete al tema 'stock'
 consumer.subscribe(['stock'])
 
 try:
     while True:
-        # Lee mensajes del tema 'stock'
         msg = consumer.poll(1.0)
 
         if msg is None:
@@ -26,13 +22,11 @@ try:
             else:
                 print('Error al recibir mensaje: {}'.format(msg.error()))
         else:
-            # Imprime el mensaje recibido con el mensaje "reponer"
             print('Reponer {}'.format(msg.value().decode('utf-8')))
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    # Cierra el consumidor al salir
     consumer.close()
 
