18c5256658cb021546878359a6329bdd9b895ce0
Andy <72534502+Sarri09@users.noreply.github.com>
2023-11-02 16:29:49 -0300

    Update inscripciones.py

--- Kafka Docker/1. Kafka Brokers/Consumer/inscripciones.py
+++ Kafka Docker/1. Kafka Brokers/Consumer/inscripciones.py
@@ -2,22 +2,18 @@ from confluent_kafka import Consumer, KafkaError
 import csv
 import os
 
-# Configura el consumidor de Kafka
 conf = {
-    'bootstrap.servers': '172.18.0.3:9092',  # Tu dirección de broker
+    'bootstrap.servers': '172.18.0.3:9092',  # LA DIRECCION DEL BROKER PUEDE CAMBIAR OJO!!! 
     'group.id': 'proceso-de-aprobacion',
     'auto.offset.reset': 'earliest'
 }
 
 c = Consumer(conf)
 
-# Suscríbete al topic 'inscripciones'
 c.subscribe(['inscripciones'])
 
-# Verifica si el archivo CSV ya existe
 csv_filename = '/usuarios.csv'
 if not os.path.exists(csv_filename):
-    # Si no existe, crea un archivo CSV con encabezados
     with open(csv_filename, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Nombre', 'Apellido', 'Nombre de Tienda', 'Direccion', 'Paid'])
@@ -28,7 +24,6 @@ def guardar_en_csv(data):
         writer = csv.writer(file)
         writer.writerow(data)
 
-# Consumir mensajes de Kafka y guardarlos en el archivo CSV
 while True:
     msg = c.poll(1.0)
 
@@ -40,22 +35,17 @@ while True:
         else:
             print('Error en el consumidor: {}'.format(msg.error()))
     else:
-        # Procesa el mensaje y extrae los datos
         message_data = msg.value().decode('utf-8')
         nombre, tienda, es_pago = message_data.split(', ')
         nombre = nombre.split(': ')[1]
         tienda = tienda.split(': ')[1]
         es_pago = es_pago.split(': ')[1]
 
-        # Divide los datos en una lista
         datos = [nombre, tienda, es_pago]
 
-        # Imprime el mensaje
         print('Solicitud aceptada:', message_data)
 
-        # Guarda los datos en el archivo CSV
         guardar_en_csv(datos)
 
-# Cierra el consumidor de Kafka al final
 c.close()
 
